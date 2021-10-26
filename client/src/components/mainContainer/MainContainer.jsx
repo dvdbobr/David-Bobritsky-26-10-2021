dd7612a15713b59b2e57d32debda565a39ed0aa0
David Bobritsky <dvdbobr@gmail.com>
2021-10-26 20:11:57 +0300

    heroku

--- client/src/components/mainContainer/MainContainer.jsx
+++ client/src/components/mainContainer/MainContainer.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import "./mainContainer.css";
 export default function MainContainer() {
+  const [liked, setLiked] = useState(false);
+  const changeLike = () => {
+    setLiked(!liked);
+  };
   return (
     <div className="mainContainer">
       <div className="mainContainerTopRow">
@@ -8,8 +15,26 @@ export default function MainContainer() {
           <h2>Tel Aviv</h2>
           <span>23C&deg;</span>
         </div>
+        <div className="likeBtn">
+          {liked ? (
+            <IconButton
+              aria-label="FavoriteBorderIcon"
+              color="error"
+              onClick={changeLike}
+            >
+              <FavoriteIcon style={{ fontSize: 42 }} />
+            </IconButton>
+          ) : (
+            <IconButton
+              aria-label="FavoriteBorderIcon"
+              color="error"
+              onClick={changeLike}
+            >
+              <FavoriteBorderIcon style={{ fontSize: 42 }} />
+            </IconButton>
+          )}
+        </div>
       </div>
-      
     </div>
   );
 }

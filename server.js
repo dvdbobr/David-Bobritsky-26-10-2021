f54745538a19476404656f4bedb7a2473f485d70
David Bobritsky <dvdbobr@gmail.com>
2021-10-26 12:45:54 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+const router = require("./server/routes");
+app.use(cors());
+const port = 5000;
+
+app.use(express.json());
+app.use("/api", router);
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+app.listen(process.env.PORT || port , () =>{
+    console.log(`Server started on port ${port}`)
+});

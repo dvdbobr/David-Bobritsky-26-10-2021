2713b7fab22fbfaf9ede40443b260652f49ce1c0
David Bobritsky <dvdbobr@gmail.com>
2021-10-26 19:31:10 +0300

    heroku

--- server.js
+++ server.js
@@ -10,14 +10,16 @@ const port = 5000;
 app.use(express.json());
 app.use("/api", router);
 if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-app.listen(process.env.PORT || port , () =>{
-    console.log(`Server started on port ${port}`)
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server started on port ${port}`)
 });
+
